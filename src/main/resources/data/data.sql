dc755c87f011461a1d8ec8e06dafd7a38de94d2a
che-97 <rue015@gmail.com>
2022-07-09 22:20:16 +0900

    docs : data.sql 삭제 (초기 db 데이터 로드 하지않음)

--- src/main/resources/data/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO user (id, display_Id) VALUES (unhex(replace('3ede0ef2-92b7-4817-a5f3-0c575361f745','-','')), 'testId1');
-
-INSERT INTO place (id, address) VALUES (unhex(replace('2e4baf1c-5acb-4efb-a1af-eddada31b00f','-','')), 'testAddress1');
